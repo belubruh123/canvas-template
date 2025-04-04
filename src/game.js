f25fd5a8224a866a547fe916cd98ce1571e74e25
belubruh123 <111185821+belubruh123@users.noreply.github.com>
2025-04-04 14:06:49 +0000

    Embeded scripts.js into index.html

---
+++ src/game.js
@@ -0,0 +1,13 @@
+
+ /* Example usage */
+ /*
+ const player = createSprite(30, 30, "red");
+ player.name = "Player";
+ player.setControlScheme({ up: 'w', down: 's', left: 'a', right: 'd' });
+ player.gravity = 1.0;
+ player.startDrawing();
+ player.penThickness = 5;
+ player.penColor = "white"
+
+ const text = createText(700, 40, "White", "Gravity Drawer", "30px Comic Sans MS");
+ */
