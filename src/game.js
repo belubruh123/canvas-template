5103e9e34fd54ffb3ab9a2a9d84891470cfa4ea9
belubruh123 <111185821+belubruh123@users.noreply.github.com>
2025-04-11 12:40:42 +0000

    Make canvas be resizeable to avoid issue.

--- src/game.js
+++ src/game.js
@@ -1,6 +1,9 @@
 
  /* Example usage */
  /*
+let debug = false;
+let canvaX = 1280; // Global variable for fixed canvas width
+let canvaY = 720; // Global variable for fixed canvas height
  const player = createSprite(30, 30, "red");
  player.name = "Player";
  player.setControlScheme({ up: 'w', down: 's', left: 'a', right: 'd' });
